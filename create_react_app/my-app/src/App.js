91ca7ef412900f697e6dc6e6d47dff815d1ad5e6
unhhyyeexx <unhhyyee66@gmail.com>
2022-06-27 19:26:46 +0900

    [react_app] styled

--- create_react_app/my-app/src/App.js
+++ create_react_app/my-app/src/App.js
@@ -1,8 +1,13 @@
+import styles from "./App.module.css"
+import Button from "./Button";
 
 function App() {
   return (
     <div>
-      <h1>Welcome back !</h1>
+      <h1 className={styles.title}>Welcome back !</h1>
+      <Button
+        text={"Continue"}
+      ></Button>
     </div>
   );
 }
