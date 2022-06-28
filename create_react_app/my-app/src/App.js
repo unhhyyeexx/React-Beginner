63de01500d712a20f1dcab77f79d020d51a07b56
unhhyyeexx <unhhyyee66@gmail.com>
2022-06-28 16:00:53 +0900

    [effect] useEffect

--- create_react_app/my-app/src/App.js
+++ create_react_app/my-app/src/App.js
@@ -1,10 +1,23 @@
 
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
+// 
+
+
+// 아래와 같은 코드는 state가 변화할 때 모든 component가 다시 실행
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1)
-  
+  console.log("i run all the time")
+
+
+  // const iRunOnlyOnce = () => {
+  //   console.log("i run only once.")
+  // }
+  // useEffect(iRunOnlyOnce, [])
+  useEffect(()=>{
+    console.log("CALL THE API")
+  }, [])
   return (
     <div>
       <h1>{counter}</h1>
